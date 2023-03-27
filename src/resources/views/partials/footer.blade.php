2a140fce102666cc1065df4e1bedc246aee8e293
vanias <vanias@autopage.inf.br>
2023-03-27 00:50:00 -0300

    ajusta layout

---
+++ src/resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="pt-5 my-5 text-muted">
+  Criado por <a href="https://www.linkedin.com/in/larissa-blem-4a1423ba/">Larissa Beatriz Freire Blem</a> &middot; &copy; {{ date('Y') }}
+</footer>
